22cf744397221418f4f804b3635bd3f97ce48bdf
Ken Van Gilbergen <mail@ken.Be>
2015-08-14 23:33:28 +0200

    Adding some Euroconsumers question.

--- README.md
+++ README.md
@@ -3,5 +3,20 @@ Playing with some interview questions.
 
 Just some code I wrote a while back for some interview riddles.
 
-Answer to the Google billboard first and second question.
+
+**Answer to the Google billboard question.**
+
 ![alt tag](https://raw.github.com/KenVanGilbergen/ken.Spikes.Math/master/ken.Spikes.QuestionBillboard/google-billboard.jpg)
+
+**Answer to the follow-up question.**
+
+f(1) =  7182818284
+f(2) =  8182845904 
+f(3) =  8747135266 
+f(4) =  7427466391 
+f(5) =  ??? 
+
+**Answer to Fibonacci question.**
+
+![alt tag](https://raw.github.com/KenVanGilbergen/ken.Spikes.Math/master/ken.Spikes.Fibonacci/fibonacci-spiral.jpg)
+
---
+++ ken.Spikes.QuestionFibonnaci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ken.Spikes.QuestionFibonnaci
+{
+    internal class Program
+    {
+        private const int Max = 15;
+
+        private static void Main()
+        {
+            Console.WriteLine("With Func");
+            Func<int, int> fib = null;
+            fib = n => n > 1 ? fib(n - 1) + fib(n - 2) : n;
+            for (var i = 0; i <= Max; i++)
+            {
+                Console.WriteLine(fib(i));
+            }
+            
+            Console.WriteLine("With recursion");
+            for (var i = 0; i <= Max; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+        }
+
+        private static int Fibonacci(int x)
+        {
+            if (x <= 1) return x;
+            return Fibonacci(x - 1) + Fibonacci(x - 2);
+        }
+    }
+}
